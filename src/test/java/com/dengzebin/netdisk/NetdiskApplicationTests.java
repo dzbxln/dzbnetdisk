e1ef1717e9bf216ad20368f13f9eea6e65ca46db
祜易 <1776252636@qq.com>
2023-05-26 15:09:07 +0800

    后端创建

---
+++ src/test/java/com/dengzebin/netdisk/NetdiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengzebin.netdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetdiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
