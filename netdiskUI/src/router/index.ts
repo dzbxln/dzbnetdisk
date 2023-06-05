e918c803444fe4283c1187e3734e9e713ebd5157
祜易 <1776252636@qq.com>
2023-06-05 16:59:46 +0800

    前端位置修改

---
+++ netdiskUI/src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Layout',
+    component: () => import('../layout/Layout.vue'),
+    redirect: "/main",
+    children: [
+      {
+        path: '/main',
+        name: 'Main',
+        component: () => import('../views/main.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
